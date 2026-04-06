ef15cae3155dacc493b4e6357f1ec9594373d583
kamil19862307 <kamil19862307@gmail.com>
2026-04-06 21:17:24 +0300

    feat: add Partner model with migration and factory

---
+++ src/app/Models/Partner.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Partner extends Model
+{
+    /** @use HasFactory<\Database\Factories\PartnerFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'logo',
+        'description',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+}
---
+++ src/database/factories/PartnerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Partner;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Partner>
+ */
+class PartnerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $name =  $this->faker->company(),
+            'slug' => null,
+            'logo' => $this->faker->imageUrl(
+                word: $name,
+            ),
+            'description' => $this->faker->realText(),
+            'active' => $this->faker->boolean(),
+        ];
+    }
+}
